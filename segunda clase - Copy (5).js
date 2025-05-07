eda0ffb5cfc89c3ff993cc08e275ea602b60dc9a
ponce1479 <ponce.tommy@gmail.com>
2025-05-06 19:22:00 -0600

    actualizacion de archivos de proyectos

---
+++ segunda clase - Copy (5).js
@@ -0,0 +1,9 @@
+console.log("Suma de dos numeros")
+let num1 =Number(prompt("Ingrese el primer numero: "))
+let num2 =Number(prompt("Ingrese el primer numero: "))
+console.log("La suma de dos numeros es: ",num1+num2)
+
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Try programiz.pro");
